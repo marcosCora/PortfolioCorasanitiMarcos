5f45c055f408952714298904e061639c54fb8f5c
MarcosPc <maagustin51@gmail.com>
2023-11-27 13:16:31 -0300

    Creacion de los componentes principales.

---
+++ src/app/components/about-me/abaut-me.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-abaut-me',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './abaut-me.component.html',
+  styleUrl: './abaut-me.component.css'
+})
+export class AbautMeComponent {
+
+}
