cfd6fc889e577b28a46cd9c494d3e6d4d7d44ea2
MarcosPc <maagustin51@gmail.com>
2023-11-27 20:25:32 -0300

    pinchando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
     templateUrl: './app.component.html',
     encapsulation: ViewEncapsulation.ShadowDom,
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, ProfileComponent]
 })
 export class AppComponent {
 
