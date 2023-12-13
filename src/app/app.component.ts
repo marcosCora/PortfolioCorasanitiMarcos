25e165a7b2db7d6cb63b22d7f4035c470d84a11d
MarcosPc <maagustin51@gmail.com>
2023-12-12 23:48:54 -0300

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { ProfileComponent } from './components/profile/profile.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AbautMeComponent } from './components/about-me/abaut-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
@@ -11,7 +15,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     templateUrl: './app.component.html',
     encapsulation: ViewEncapsulation.ShadowDom,
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, ProfileComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, ProfileComponent, SkillsComponent, AbautMeComponent, ProjectsComponent, FooterComponent]
 })
 export class AppComponent {
 
