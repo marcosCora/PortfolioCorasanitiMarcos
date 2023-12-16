5df44a118720abb3e15e3ef0668c157cbb02372f
MarcosPc <maagustin51@gmail.com>
2023-12-16 19:31:57 -0300

    tratando de enrutar, cambios en el css del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'about-me', component: ProfileComponent},
+    {path: 'skills', component: SkillsComponent},
+    {path: 'projects', component: ProjectsComponent}
+];
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
