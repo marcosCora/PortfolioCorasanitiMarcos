b9ddd8153a598922eb0d4a2109b1c25f169d1c7f
MarcosNotebook <maagustin51@gmail.com>
2023-12-18 13:14:00 -0300

    tratando de enrutar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -11,4 +11,15 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class NavBarComponent {
 
+  scrollSkills(){
+    let sectionSkills = document.getElementById('skills');
+    if(sectionSkills){
+      sectionSkills.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' 
+      });
+    }
+  }
+
+
 }
