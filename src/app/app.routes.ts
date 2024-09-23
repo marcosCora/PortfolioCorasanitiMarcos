b28404ca4a24fac97f7fbaa0da9532402a88d68f
Marcos Corasaniti N <marcoscorasaniti07@gmail.com>
2024-09-23 16:36:28 -0300

    about me casi finalizado, comenzando con proyectos y experiencias.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     {path: 'home', component: ProfileComponent},
     {path: 'about-me', component: AboutMeComponent},
-    {path: 'skills', component: SkillsComponent},
-    {path: 'projects', component: ProjectsComponent}
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'contact', component: ContactComponent}
 ];
