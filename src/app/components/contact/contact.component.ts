28fc418ab6caeae1fbf78d72b4b8a4dbdf255820
Marcos Corasaniti N <marcoscorasaniti07@gmail.com>
2024-09-30 21:01:08 -0300

    problemas de cors, intentado hacer el envio de mails

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Resend } from 'resend';
+import { log } from 'console';
 
 @Component({
   selector: 'app-contact',
@@ -10,4 +12,62 @@ import { CommonModule } from '@angular/common';
 })
 export class ContactComponent {
 
+  public resend = new Resend('re_123456789');
+
+  
+  async send(){
+    console.log(this.resend.apiKeys);
+    //await this.sendEmail('marcoscorasaniti07@gmail.com', 'prueba', '<p>hola probando</p>');
+    this.function();
+  }
+
+  async function () {
+    const { data, error } = await this.resend.emails.send({
+      from: 'maagustin51@gmail.com',
+      to: ['marcoscorasaniti07@gmail.com'],
+      subject: 'Hello World',
+      html: '<strong>It works!</strong>',
+    });
+  
+    if (error) {
+      return console.error({ error });
+    }
+  
+    console.log({ data });
+  };
+
 }
+  /* async sendEmail(to: string, subject: string, html: string) {
+    /*try {
+        console.log("holaaa");
+        
+         const data = await this.resend.emails.send({
+          from: 'Acme <onboarding@resend.dev>',
+          to: ['maagustin51@gmail.com'],
+          subject: 'Hello World',
+          html: '<strong>It works!</strong>',
+        }); 
+        try {
+          const response = await fetch('https://api.resend.com/emails', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              from: 'maagustin51@gmail.com',
+              to,
+              subject,
+              html,
+            }),
+            mode: 'no-cors', // Development only
+          });
+
+        console.log("enviado");
+    } catch (error) {
+        console.error(error);
+    }
+} */
+
+
+
+
