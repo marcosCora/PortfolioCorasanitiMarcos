c0142b1882cd4bff411a6b250772d01a1b84b43c
MarcosPc <maagustin51@gmail.com>
2024-10-07 09:46:48 -0300

    envio de email funcionando, falta personalizar

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,8 @@
+
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -9,5 +12,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_1dmbec3', 'template_txcnghg', e.target as HTMLFormElement, {
+        publicKey: 'Iprp76_bIxSNJRsj3',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
+
 
 }
