fde2e4d99afb7378894f76bae0e86bd8e13b3dc7
Marcos Corasaniti N <marcoscorasaniti07@gmail.com>
2024-10-17 12:43:15 -0300

    finalizando funcionalidad edl formulario

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 import { FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -12,15 +12,47 @@ import { FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angula
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent  implements OnInit{
 
-  formEmail : FormGroup = new FormGroup({
-    nombre : new FormControl(''),
-    email : new FormControl(''),
-    mensaje : new FormControl('')
-  })
+  formEmail !: FormGroup;
+
+  public ngOnInit(): void {
+    this.formEmail = new FormGroup({
+      name : new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email : new FormControl('', [Validators.required, Validators.email]),
+      message : new FormControl('', [Validators.required, Validators.minLength(1)])
+    })
+  }
 
 
+   sendEmail() {
+
+    console.log(this.formEmail);
+    
+    if(!this.formEmail.invalid){
+      let email = {
+        name : this.formEmail.controls['name'].value,
+        email : this.formEmail.controls['email'].value,
+        message : this.formEmail.controls['message'].value,
+      };
+
+      console.log(email);
+    }
+    /* 
+    emailjs
+      .sendForm('service_1dmbec3', 'template_txcnghg', e.target as HTMLFormElement, {
+        publicKey: 'Iprp76_bIxSNJRsj3',
+        
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+   */}
   
   
 
